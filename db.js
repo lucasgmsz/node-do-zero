38fd15d07dc5bd5c60154269938ea2472f8ba4ec
Lucas <lucas_gomesdesouza@outlook.com>
2024-12-18 18:01:58 -0300

    api node

--- db.js
+++ db.js
@@ -4,4 +4,4 @@ import postgres from "postgres";
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 const URL = `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`;
 
-const sql = postgres(URL, { ssl: "require" });
+export const sql = postgres(URL, { ssl: "require" });
